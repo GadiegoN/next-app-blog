d55a1b02545df9d9c613d888b4b633a896404176
GadiegoN <ngadiego@gmail.com>
2024-09-26 01:37:58 -0300

    feat: update getPosts to use no-cache strategy

--- app/_actions/get-posts.ts
+++ app/_actions/get-posts.ts
@@ -1,14 +1,11 @@
 import { PostProps } from "../_types/posts";
 
 export async function getPosts(): Promise<PostProps[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, {
-        cache: 'no-cache',
-        next: { revalidate: 60 }
-    });
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, { cache: 'no-cache' })
 
     if (!response.ok) {
-        throw new Error('Falied to fetch posts.');
+        throw new Error('Falied to fetch posts.')
     }
 
-    return response.json();
+    return response.json()
 }
